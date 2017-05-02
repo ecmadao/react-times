bfa78e368b2bd46c8c786e760384e225b132ca9c
Carlo DiCelico <carlodicelico@me.com>
2017-05-02 14:08:25 -0400

    remove junk file

--- scratch.js
+++
@@ -1,9 +0,0 @@
-
-// We need both a 12h and 24h formatted time here in the parent
-// so we can infer the meridiem and pass it down to the children
-const time = (defaultTime && meridiem) ? `${defaultTime}${meridiem}` : defaultTime;
-const timeIn12Hour = timeHelper.time(time, 12);
-const timeIn24Hour = timeHelper.time(defaultTime);
-
-const [hour, minute] = timeIn24Hour;
-const meridiem = timeIn12Hour[2]; // AM or PM
