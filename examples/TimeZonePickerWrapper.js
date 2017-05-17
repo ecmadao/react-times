128322a84fb2c08357790b01f923c1fe2d39dcec
Erin Doyle <edoyle@cashstar.com>
2017-05-17 08:43:08 -0400

    Refactored the TimeZonePickerWrapper to now use the TimeZone component

--- examples/TimeZonePickerWrapper.js
+++ examples/TimeZonePickerWrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import TimeZonePicker from '../src/components/TimeZone/TimeZonePicker';
+import TimeZone from '../src/components/TimeZone';
 import timeHelper from '../src/utils/time';
 import languageHelper from '../src/utils/language';
 
@@ -14,13 +14,8 @@ class TimeZonePickerWrapper extends React.Component {
     const {timezone} = this.props;
 
     this.state = {
-      focused: false,
       timezone
     };
-
-    this.onClearFocus = this.onClearFocus.bind(this);
-    this.handleFocusedChange = this.handleFocusedChange.bind(this);
-    this.handleTimezoneChange = this.handleTimezoneChange.bind(this);
   }
 
   languageData() {
@@ -28,39 +23,19 @@ class TimeZonePickerWrapper extends React.Component {
     return Object.assign({}, languageHelper.get(language), phrases);
   }
 
-  onClearFocus() {
-    this.setState({focused: false});
-  }
-
-  handleFocusedChange() {
-    const {focused} = this.state;
-    this.setState({focused: !focused});
-  }
-
-  handleTimezoneChange(timezone) {
-    this.setState({timezone});
-  }
-
   render() {
-    const {focused, timezone} = this.state;
+    const {timezone} = this.state;
 
     return (
       <div>
         <div className="outside_container active">
           <div className='time_picker_modal_container'>
-            <div className='time_picker_modal_footer'
-              onClick={this.handleFocusedChange}
-            >
-              <span className='time_picker_modal_footer_timezone'>{timezone.zoneName} {timezone.zoneAbbr}</span>
-            </div>
+            <TimeZone
+              phrases={this.languageData()}
+              timezone={timezone}
+              timezoneIsEditable={true}
+            />
           </div>
-          <TimeZonePicker
-            {...this.props}
-            phrases={this.languageData()}
-            focused={focused}
-            onClearFocus={this.onClearFocus}
-            handleTimezoneChange={this.handleTimezoneChange}
-          />
         </div>
       </div>
     )
