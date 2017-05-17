a120538adc53f4587b1f60c46b621a7f8242ec3b
Carlo DiCelico <carlodicelico@me.com>
2017-05-17 17:28:51 -0400

    what. the. fuck.

---
+++ stories/Timezones.js
@@ -0,0 +1,55 @@
+import '../css/material/default.css';
+
+import {text, withKnobs} from '@kadira/storybook-addon-knobs';
+
+import React from 'react';
+import TimePickerWrapper from '../examples/TimePickerWrapper';
+import TimezonePickerWrapper from '../examples/TimezonePickerWrapper';
+import {storiesOf} from '@kadira/storybook';
+import timeHelper from '../src/utils/time.js';
+
+const tzForCity = timeHelper.tzForCity('Kuala Lumpur');
+
+storiesOf('Timezones', module)
+  .addDecorator(withKnobs)
+  .addWithInfo('with default (detected) timezone', () => (
+    <TimePickerWrapper
+      showTimezone={true} />
+  ))
+  .addWithInfo('with default (custom) timezone', () => (
+    <TimePickerWrapper
+      timezone={tzForCity.zoneName}
+      showTimezone={true} />
+  ))
+  .addWithInfo('with timezone search', () => {
+    return (
+      <TimePickerWrapper
+        showTimezone={true}
+        timezoneIsEditable={true}
+      />
+    )
+  })
+  .addWithInfo('with 12 hour (custom) time', () => {
+    return (
+      <TimePickerWrapper
+        timeMode="12"
+        defaultTime="13:15"
+        showTimezone={true}
+        timezoneIsEditable={true}
+      />
+    )
+  })
+  .addWithInfo('with dark theme', () => {
+    return (
+      <TimePickerWrapper
+        colorPalette="dark"
+        showTimezone={true}
+        timezoneIsEditable={true}
+      />
+    )
+  })
+  .addWithInfo('with timezone picker', () => {
+    return (
+      <TimezonePickerWrapper />
+    )
+  });
