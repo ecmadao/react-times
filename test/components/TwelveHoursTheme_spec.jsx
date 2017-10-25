e8f4d5036dcb66ed8d723ec649a7fb8e85cbfa3a
ecmadao <wlec@outlook.com>
2017-10-26 01:31:45 +0800

    WHAT: Fix failed test
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- test/components/TwelveHoursTheme_spec.jsx
+++ test/components/TwelveHoursTheme_spec.jsx
@@ -42,9 +42,9 @@ describe('TwelveHoursMode', () => {
     const handleMeridiemChange = sinon.stub();
     const wrapper = shallow(
       <TwelveHoursMode
+        focused
         hour={'01'}
         minute={'45'}
-        focused={true}
         meridiem={'AM'}
         phrases={phrases}
         handleHourChange={handleHourChange}
@@ -53,22 +53,28 @@ describe('TwelveHoursMode', () => {
       />
     );
     it('should handleHourPointerClick', () => {
-      wrapper.instance().handleHourPointerClick(3, 90);
+      wrapper.instance().handleHourPointerClick({
+        time: 3,
+        pointerRotate: 90
+      });
       expect(wrapper.state().hourPointerRotate).to.equal(90);
       expect(handleHourChange.callCount).to.equal(1);
     });
 
     it('should handleHourPointerClick', () => {
-      wrapper.instance().handleMinutePointerClick(30, 180);
+      wrapper.instance().handleMinutePointerClick({
+        time: 30,
+        pointerRotate: 180
+      });
       expect(wrapper.state().minutePointerRotate).to.equal(180);
       expect(handleMinuteChange.callCount).to.equal(1);
     });
 
     it('should handleMeridiemChange', () => {
-      wrapper.instance().handleMeridiemChange('AM');
-      expect(handleMeridiemChange.callCount).to.equal(0);
-      wrapper.instance().handleMeridiemChange('PM');
+      wrapper.instance().handleMeridiemChange();
       expect(handleMeridiemChange.callCount).to.equal(1);
+      wrapper.instance().handleMeridiemChange();
+      expect(handleMeridiemChange.callCount).to.equal(2);
     });
   });
 
