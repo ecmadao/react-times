e8f178a991753411d1d61a5b145aa7dcf9c28207
rich tillman <richtillman@richards-MacBook-Pro.local>
2017-12-11 16:34:58 -0800

    prop to disable outside click

--- stories/TimePicker.js
+++ stories/TimePicker.js
@@ -6,57 +6,19 @@ import TimePickerWrapper from '../examples/TimePickerWrapper';
 
 storiesOf('Default TimePicker', module)
   .addDecorator(withKnobs)
-  .addWithInfo('basic', () => (
-    <TimePickerWrapper />
-  ))
+  .addWithInfo('basic', () => <TimePickerWrapper />)
   .addWithInfo('with default time', () => {
     const aDefaultTime = text('set default time', '22:10');
-    return (
-      <TimePickerWrapper
-        defaultTime={aDefaultTime}
-      />
-    );
+    return <TimePickerWrapper defaultTime={aDefaultTime} />;
   })
-  .addWithInfo('focused at setup', () => (
-    <TimePickerWrapper
-      focused
-    />
-  ))
-  .addWithInfo('not auto change time panel', () => (
-    <TimePickerWrapper
-      autoMode={false}
-    />
-  ))
-  .addWithInfo('undraggable', () => (
-    <TimePickerWrapper
-      draggable={false}
-    />
-  ))
-  .addWithInfo('custom minute step', () => (
-    <TimePickerWrapper
-      autoMode={false}
-      minuteStep={1}
-    />
-  ))
-  .addWithInfo('limit drag', () => (
-    <TimePickerWrapper
-      limitDrag
-      autoMode={false}
-      minuteStep={1}
-    />
-  ))
-  .addWithInfo('custom HH-MM format', () => (
-    <TimePickerWrapper
-      timeFormat={'HH-MM'}
-    />
-  ))
-  .addWithInfo('custom H-M format', () => (
-    <TimePickerWrapper
-      timeFormat={'H-M'}
-    />
-  ))
+  .addWithInfo('focused at setup', () => <TimePickerWrapper focused />)
+  .addWithInfo('not auto change time panel', () => <TimePickerWrapper autoMode={false} />)
+  .addWithInfo('undraggable', () => <TimePickerWrapper draggable={false} />)
+  .addWithInfo('custom minute step', () => <TimePickerWrapper autoMode={false} minuteStep={1} />)
+  .addWithInfo('limit drag', () => <TimePickerWrapper limitDrag autoMode={false} minuteStep={1} />)
+  .addWithInfo('custom HH-MM format', () => <TimePickerWrapper timeFormat={'HH-MM'} />)
+  .addWithInfo('custom H-M format', () => <TimePickerWrapper timeFormat={'H-M'} />)
   .addWithInfo('custom time formatter', () => (
-    <TimePickerWrapper
-      timeFormatter={({ hour, minute }) => `${hour} & ${minute}`}
-    />
-  ));
+    <TimePickerWrapper timeFormatter={({ hour, minute }) => `${hour} & ${minute}`} />
+  ))
+  .addWithInfo('Disable outside click', () => <TimePickerWrapper outsideClick={false} />);
