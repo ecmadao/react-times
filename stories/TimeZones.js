48833c1d73b81a234935c1cc6b1df3f1fdb05c22
Ivo Tripunovic <tripunovic@yahoo.com>
2018-03-02 12:56:18 +0100

    WHAT: Fixing errors on pre push hook
    WHY:
    * So I could push the branch to remote repository
    
    HOW:
    * By prettifing code

--- stories/TimeZones.js
+++ stories/TimeZones.js
@@ -1,11 +1,11 @@
 import '../css/material/default.css';
 
-import {text, withKnobs} from '@kadira/storybook-addon-knobs';
+import { withKnobs } from '@kadira/storybook-addon-knobs';
 
 import React from 'react';
 import TimePickerWrapper from '../examples/TimePickerWrapper';
 import TimezonePickerWrapper from '../examples/TimezonePickerWrapper';
-import {storiesOf} from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import timeHelper from '../src/utils/time.js';
 
 const tzForCity = timeHelper.tzForCity('Kuala Lumpur');
@@ -13,43 +13,23 @@ const tzForCity = timeHelper.tzForCity('Kuala Lumpur');
 storiesOf('Timezones', module)
   .addDecorator(withKnobs)
   .addWithInfo('with default (detected) timezone', () => (
-    <TimePickerWrapper
-      showTimezone={true} />
+    <TimePickerWrapper showTimezone />
   ))
   .addWithInfo('with default (custom) timezone', () => (
-    <TimePickerWrapper
-      timezone={tzForCity.zoneName}
-      showTimezone={true} />
+    <TimePickerWrapper timezone={tzForCity.zoneName} showTimezone />
   ))
-  .addWithInfo('with timezone search', () => {
-    return (
-      <TimePickerWrapper
-        showTimezone={true}
-        timezoneIsEditable={true}
-      />
-    )
-  })
-  .addWithInfo('with 12 hour (custom) time', () => {
-    return (
-      <TimePickerWrapper
-        timeMode="12"
-        defaultTime="13:15"
-        showTimezone={true}
-        timezoneIsEditable={true}
-      />
-    )
-  })
-  .addWithInfo('with dark theme', () => {
-    return (
-      <TimePickerWrapper
-        colorPalette="dark"
-        showTimezone={true}
-        timezoneIsEditable={true}
-      />
-    )
-  })
-  .addWithInfo('with timezone picker', () => {
-    return (
-      <TimezonePickerWrapper />
-    )
-  });
+  .addWithInfo('with timezone search', () => (
+    <TimePickerWrapper showTimezone timezoneIsEditable />
+  ))
+  .addWithInfo('with 12 hour (custom) time', () => (
+    <TimePickerWrapper
+      timeMode="12"
+      defaultTime="13:15"
+      showTimezone
+      timezoneIsEditable
+    />
+  ))
+  .addWithInfo('with dark theme', () => (
+    <TimePickerWrapper colorPalette="dark" showTimezone timezoneIsEditable />
+  ))
+  .addWithInfo('with timezone picker', () => <TimezonePickerWrapper />);
